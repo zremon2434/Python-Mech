8174d13683cb8e01e7a72f533a944761250d8d28
ZR Emon (EmoMech) <140493256+zremon2434@users.noreply.github.com>
2023-09-02 00:45:21 +0600

    Add files via upload

---
+++ practice(emomech).py
@@ -0,0 +1,11 @@
+Relation = [['Boy1', 'Girl1'], ['emon', 'still nt found:('], ['Boy2', 'Girl2'], ['Boy3', 'Girl3']]
+
+x = input('Tell me the address of Boy1.')
+
+
+
+u = input('What about emon GF?')
+
+print('DAMMM! Bro you got me.', y, Relation[0][1], 'is.')
+print(z, 'Bro.')
+print('Sorry to say but', Relation[1][1])
