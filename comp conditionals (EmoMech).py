84ebe7401a5b3d3555aefabc3481c12aba019fc5
ZR Emon (EmoMech) <140493256+zremon2434@users.noreply.github.com>
2023-09-06 00:43:23 +0600

    Add files via upload

---
+++ comp conditionals (EmoMech).py
@@ -0,0 +1,11 @@
+userinput = float(input('Emon tell me how many GF do you have back then? '))
+print('Shitt!! you have', userinput, 'GF only')
+
+if (userinput < 5 or userinput > 10):
+    print('You are such a useless.')
+
+if (userinput > 5 and userinput < 10):
+    print('Whatever this is perfect.')
+
+if (userinput == 5 or userinput == 10):
+    print('What the helll are you doing brohhh!!')
