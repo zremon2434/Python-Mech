73cac55a5124a621026f7c2408d44b97e29425ea
ZR Emon (EmoMech) <140493256+zremon2434@users.noreply.github.com>
2023-10-10 23:41:54 +0600

    Update Find circ. and area of a circle (EmoMech).py

--- Find circ. and area of a circle (EmoMech).py
+++ Find circ. and area of a circle (EmoMech).py
@@ -1,3 +1,6 @@
+#project:Find area of a circle
+#Author:Emon
+
 Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> radius =2
