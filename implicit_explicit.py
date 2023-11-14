18ece58b184efd60dddd41ca2c67229fd44e42d3
ZR Emon (EmoMech) <140493256+zremon2434@users.noreply.github.com>
2023-11-14 16:52:37 +0600

    Add files via upload

---
+++ implicit_explicit.py
@@ -0,0 +1,19 @@
+#explicit typecasting
+
+a="4"
+b="2.3"
+print(int(a)+float(b))
+print(int(a)/float(b))
+print(int(a)**float(b))
+print(int(a)%float(b))
+
+#implicit typecasting
+
+a=2
+b=6.7
+c="emon"
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(c,"Emon","EMON",sep="=")
