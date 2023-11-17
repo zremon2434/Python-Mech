fe5968a54b742f016a155bec4a58e4eb5e78db35
ZR Emon (EmoMech) <140493256+zremon2434@users.noreply.github.com>
2023-11-18 01:12:57 +0600

    Update Find circ. and area of a circle (EmoMech).py

--- Find circ. and area of a circle (EmoMech).py
+++ Find circ. and area of a circle (EmoMech).py
@@ -1,12 +1,18 @@
-#project:Find area of a circle
-#Author:Emon
+# Project: Find area of a circle
+# Author: Emon
 
-Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> radius =2
->>> circumference =2*3.14*radius
->>> print('A circle of Radius',radius,'has circ. of',circumference)
-A circle of Radius 2 has circ. of 12.56
->>> area =3.14*radius**2
->>> print('A circle of Radius',radius,'has an area of',area)
-A circle of Radius 2 has an area of 12.56
+# Define the radius of the circle
+radius = 2
+
+# Calculate the circumference using the formula: 2 * pi * radius
+circumference = 2 * 3.14 * radius
+
+# Print the circumference of the circle
+print('A circle of Radius', radius, 'has circ. of', circumference)
+
+# Calculate the area using the formula: pi * radius^2
+area = 3.14 * radius**2
+
+# Print the area of the circle
+print('A circle of Radius', radius, 'has an area of', area)
+
