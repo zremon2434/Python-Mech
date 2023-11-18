9e022f896d37da06952e7ff153a7950e8b3c6692
ZR Emon (EmoMech) <140493256+zremon2434@users.noreply.github.com>
2023-11-19 00:45:20 +0600

    Update practice(emomech).py

--- practice(emomech).py
+++ practice(emomech).py
@@ -1,13 +1,15 @@
-#Author:Emon
+# Author: Emon
 
+# Define relationships between boys and girls
 Relation = [['Boy1', 'Girl1'], ['emon', 'still nt found:('], ['Boy2', 'Girl2'], ['Boy3', 'Girl3']]
 
+# Get the address of Boy1 from the user
 x = input('Tell me the address of Boy1.')
 
-
-
+# Ask the user about Emon's girlfriend
 u = input('What about emon GF?')
 
+# Print relationship details based on user input
 print('DAMMM! Bro you got me.', y, Relation[0][1], 'is.')
 print(z, 'Bro.')
 print('Sorry to say but', Relation[1][1])
