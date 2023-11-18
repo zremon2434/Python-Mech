25f0954401cfa71701d617b4e792d7635fc181e0
ZR Emon (EmoMech) <140493256+zremon2434@users.noreply.github.com>
2023-11-19 00:44:02 +0600

    Update comp conditionals (EmoMech).py

--- comp conditionals (EmoMech).py
+++ comp conditionals (EmoMech).py
@@ -1,13 +1,19 @@
-#Author:Emon
+# Author: Emon
 
+# Get user input on the number of girlfriends
 userinput = float(input('Emon tell me how many GF do you have back then? '))
+
+# Print the number of girlfriends
 print('Shitt!! you have', userinput, 'GF only')
 
+# Check if the number is less than 5 or greater than 10
 if (userinput < 5 or userinput > 10):
     print('You are such a useless.')
 
+# Check if the number is between 5 and 10
 if (userinput > 5 and userinput < 10):
     print('Whatever this is perfect.')
 
+# Check if the number is exactly 5 or 10
 if (userinput == 5 or userinput == 10):
     print('What the helll are you doing brohhh!!')
